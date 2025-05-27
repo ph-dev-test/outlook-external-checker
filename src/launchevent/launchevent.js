b5014b10747ceab29c681ffd7a8cb5025ad6f6ca
ph-dev-test <ph-dev-test@bizwind.co.jp>
2025-05-27 11:27:09 +0800

    Update launchevent.js

--- src/launchevent/launchevent.js
+++ src/launchevent/launchevent.js
@@ -3,7 +3,7 @@
  * See LICENSE in the project root for license information.
  */
 
-const customerDomain = "@your-domain.com";
+const customerDomain = "@bizwind.co.jp";
 
 function onMessageSendHandler(event) {
   let externalRecipients = [];
