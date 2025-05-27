7355fadab1158a8930dc176b53cb3b028913d10d
ph-dev-test <ph-dev-test@bizwind.co.jp>
2025-05-27 15:55:21 +0800

    Update launchevent.js

--- src/launchevent/launchevent.js
+++ src/launchevent/launchevent.js
@@ -1,40 +1,40 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
 
 const customerDomain = "@bizwind.co.jp";
 
 function onMessageSendHandler(event) {
   let externalRecipients = [];
-
   Office.context.mailbox.item.to.getAsync((asyncResult) => {
     if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
-      const recipients = asyncResult.value;
-
+      var recipients = asyncResult.value;
       recipients.forEach((recipient) => {
         if (!recipient.emailAddress.includes(customerDomain)) {
           externalRecipients.push(recipient.emailAddress);
         }
-      });
 
-      if (externalRecipients.length > 0) {
-        event.completed({
-          allowEvent: false,
-          errorMessage:
-            "You are sending this email to external recipients:\n\n" +
-            externalRecipients.join("\n") +
-            "\n\nAre you sure you want to send it?",
-        });
-      } else {
-        event.completed({ allowEvent: true });
-      }
+        if (externalRecipients.length > 0) {
+          event.completed({
+            allowEvent: false,
+            errorMessage:
+              "The mail includes some external recipients, are you sure you want to send it?\n\n" +
+              externalRecipients.join("\n") +
+              "\n\nClick Send to send the mail anyway.",
+          });
+        } else {
+          event.completed({ allowEvent: true });
+        }
+      });
     } else {
-      // If there's an error retrieving recipients, allow sending to avoid blocking
       event.completed({ allowEvent: true });
     }
   });
 }
 
-// Associate the handler with the event
+// IMPORTANT: To ensure your add-in is supported in the Outlook client on Windows, remember to map the event handler name specified in the manifest's LaunchEvent element to its JavaScript counterpart.
+// 1st parameter: FunctionName of LaunchEvent in the manifest; 2nd parameter: Its implementation in this .js file.
 if (Office.context.platform === Office.PlatformType.PC || Office.context.platform == null) {
   Office.actions.associate("onMessageSendHandler", onMessageSendHandler);
 }
